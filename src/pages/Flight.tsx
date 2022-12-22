3ae4ed2e2a3b785e72950d763f0aefbf1bf9c6f5
Rogalio mouchel <you@example.com>
2022-12-22 14:09:48 +0100

    test

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -4,26 +4,18 @@ import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
 
 const Flight = () => {
-  // how to ignore typescript on a component
+  useEffect((): void => {
+    if (window.navigator.cookieEnabled) {
+      console.log("Cookies are enabled in the user's browser");
 
-  //  useEffect((): void => {
-  //   if (window.navigator.permissions && window.navigator.permissions.query) {
-  //     window.navigator.permissions.query({ name: 'cookie' }).then((result) => {
-  //       if (result.state === 'granted') {
-  //         // Set the cookie
-  //         document.cookie = "name=value;path=/";
-  //       } else {
-  //         // Do not set the cookie
-  //       }
-  //     });
-  //   } else {
-  //     // Fallback for unsupported browsers
-  //     // Set the cookie without requesting permission
-  //     document.cookie = "name=value;path=/";
-  //   }
-  // }, []);
-
-  // console.log(window.navigator.permissions.query({ name: "cookies" }));
+      // Cookies are enabled in the user's browser
+      // You can use cookies in your React app
+    } else {
+      console.log("Cookies are disabled in the user's browser");
+      // Cookies are disabled in the user's browser
+      // You should not use cookies in your React app
+    }
+  }, []);
 
   return (
     <div className="relative dark:bg-c1">
