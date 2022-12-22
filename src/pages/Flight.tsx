84714ca28bc0c3b306d68ed28be916af7276121c
Rogalio mouchel <you@example.com>
2022-12-22 15:57:52 +0100

    test

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -1,9 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HeroFlight from "../components/HeroFlight";
 import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
 
 const Flight = () => {
+  const requestStorageAccess = (setIsGranted: (value: boolean) => void) => {
+    if (typeof document.requestStorageAccess === "function") {
+      document.requestStorageAccess().then((result: any) => {
+        setIsGranted(result);
+      });
+    } else {
+      // Fallback for unsupported browsers
+      // You can use localStorage or indexedDB without requesting permission
+      setIsGranted(true);
+    }
+  };
+
+  const [isGranted, setIsGranted] = useState(false);
+
+  useEffect((): void => {
+    requestStorageAccess(setIsGranted);
+  }, []);
+
+  if (isGranted) {
+    // Access to storage has been granted
+    // You can now use localStorage or indexedDB
+  } else {
+    // Access to storage has not been granted yet
+    // You should not use localStorage or indexedDB
+    return (
+      <div>
+        <p>Would you like to allow this app to access your storage?</p>
+        <button onClick={() => requestStorageAccess(setIsGranted)}>Yes</button>
+        <button onClick={() => setIsGranted(false)}>No</button>
+      </div>
+    );
+  }
+
   return (
     <div className="relative dark:bg-c1">
       <HeroFlight />
