6fd6b3733f7fcfe4e0e1430e9ff621a55ac42ae6
Rogalio mouchel <you@example.com>
2022-12-22 15:30:00 +0100

    tesft

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -4,18 +4,7 @@ import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
 
 const Flight = () => {
-  useEffect((): void => {
-    if (window.navigator.cookieEnabled) {
-      console.log("Cookies are enabled in the user's browser");
-
-      // Cookies are enabled in the user's browser
-      // You can use cookies in your React app
-    } else {
-      console.log("Cookies are disabled in the user's browser");
-      // Cookies are disabled in the user's browser
-      // You should not use cookies in your React app
-    }
-  }, []);
+  console.log(document.hasStorageAccess);
 
   return (
     <div className="relative dark:bg-c1">
