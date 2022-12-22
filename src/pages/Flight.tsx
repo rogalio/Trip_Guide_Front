6e3ded0a6ad76a4af9df2849846c24ec47910080
Rogalio mouchel <you@example.com>
2022-12-22 16:19:39 +0100

    test

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import HeroFlight from "../components/HeroFlight";
 import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
-import TestSafari from "../components/testSafari";
+import TestSafari from "../components/TestSafari";
 
 const Flight = () => {
   return (
