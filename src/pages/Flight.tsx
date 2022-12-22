d81cb41f561469459f8ed53a1645d7ee617e6841
Rogalio mouchel <you@example.com>
2022-12-22 12:58:58 +0100

    add cookie consent

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
 import HeroFlight from "../components/HeroFlight";
-import axios from "axios";
 import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
 
 const Flight = () => {
-  axios.defaults.withCredentials = true;
+  // how to ignore typescript on a component
+
+  //  useEffect((): void => {
+  //   if (window.navigator.permissions && window.navigator.permissions.query) {
+  //     window.navigator.permissions.query({ name: 'cookie' }).then((result) => {
+  //       if (result.state === 'granted') {
+  //         // Set the cookie
+  //         document.cookie = "name=value;path=/";
+  //       } else {
+  //         // Do not set the cookie
+  //       }
+  //     });
+  //   } else {
+  //     // Fallback for unsupported browsers
+  //     // Set the cookie without requesting permission
+  //     document.cookie = "name=value;path=/";
+  //   }
+  // }, []);
+
+  // console.log(window.navigator.permissions.query({ name: "cookies" }));
 
   return (
     <div className="relative dark:bg-c1">
--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -2,10 +2,22 @@ import { AuthModal } from "../util/AuthModal";
 import DestinationCard from "../components/DestinationCard";
 import Hero from "../components/HeroHotel";
 import TopTourCard from "../components/TopTourCard";
+import CookieConsent from "react-cookie-consent";
 
 const Home = () => {
   return (
     <div className=" dark:bg-c1">
+      <CookieConsent
+        location="bottom"
+        buttonText="Yes"
+        cookieName="CookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+      </CookieConsent>
       <AuthModal />
       <Hero />
       <DestinationCard />
