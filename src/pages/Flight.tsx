151be500fdaee3a953fed486c7c38edb0262a8a7
Rogalio mouchel <you@example.com>
2022-12-22 15:49:51 +0100

    test

--- src/pages/Flight.tsx
+++ src/pages/Flight.tsx
@@ -4,31 +4,6 @@ import DestinationCard from "../components/DestinationCard";
 import TopTourCard from "../components/TopTourCard";
 
 const Flight = () => {
-  const requestStorageAccess = () => {
-    if (typeof document.requestStorageAccess === "function") {
-      document.requestStorageAccess().then((result: any) => {
-        if (result) {
-          console.log("Access to storage has been granted");
-
-          // Access to storage has been granted
-          // You can now use localStorage or indexedDB
-        } else {
-          console.log("Access to storage has been denied");
-          // Access to storage has been denied
-          // You should not use localStorage or indexedDB
-        }
-      });
-    } else {
-      console.log("Fallback for unsupported browsers");
-      // Fallback for unsupported browsers
-      // You can use localStorage or indexedDB without requesting permission
-    }
-  };
-
-  useEffect((): void => {
-    requestStorageAccess();
-  }, []);
-
   return (
     <div className="relative dark:bg-c1">
       <HeroFlight />
